a02f4e442dce32e4d92f805985552566f2455c34
James Cox <j-cox03@hotmail.com>
2017-06-17 14:33:23 +0100

    Moved SqlRunner tests into new DatabaseAssistant spec file. Added tests for where clause builder (not yet written)

---
+++ app/spec/db/database_assistant_spec.rb
@@ -0,0 +1,43 @@
+require ("minitest/autorun")
+require ("minitest/rg")
+require_relative ("../../db/database_assistant.rb")
+
+class FizzBuzzTest < MiniTest::Test
+
+  def test_build_column_string()
+    columns = ["name", "age", "height", "hair_colour"]
+    expected = "name, age, height, hair_colour"
+    result = SqlRunner.build_column_string(columns)
+    assert_equal(expected, result)
+  end
+
+  def test_build_placeholder_string()
+    columns = ["name", "age", "height", "hair_colour"]
+    expected = "$1, $2, $3, $4"
+    result = SqlRunner.build_placeholder_string(columns)
+    assert_equal(expected, result)
+  end
+
+  def test_build_where_clause__1_value()
+    values = {
+      "id" => 50
+    }
+    expected = "WHERE id = 50"
+    result = build_where_clause(values)
+    assert_equal(expected, result)
+  end
+
+  def test_build_where_clause__5_values()
+    values = {
+      "id" => 50,
+      "name" => "James",
+      "age" => 24,
+      "height" => 180,
+      "pet" => "teddy"
+    }
+    expected = "WHERE id = 50 AND name = 'James' AND age = 24 AND height = 180 AND pet = 'teddy'"
+    result = build_where_clause(values)
+    assert_equal(expected, result)
+  end
+
+end
