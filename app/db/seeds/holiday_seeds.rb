2f31c1c2922a65b98f48065372971aecc63baa07
James Cox <j-cox03@hotmail.com>
2017-06-17 21:41:39 +0100

    Made randomly generated prices more believable

--- app/db/seeds/holiday_seeds.rb
+++ app/db/seeds/holiday_seeds.rb
@@ -8,13 +8,33 @@ def make_holidays(travel_agents, hotels)
   holidays = []
 
   20.times() do |num|
+
+    agent = travel_agents.sample
+    hotel = hotels.sample
+    cost = random_cost(hotel)
+
     holidays[num] = Holiday.new({
-      "travel_agent_id" => (travel_agents.sample).id,
-      "hotel_id" => (hotels.sample).id,
-      "cost_per_person" => rand((400..2_000)),
+      "travel_agent_id" => agent.id,
+      "hotel_id" => hotel.id,
+      "cost_per_person" => cost,
       "nights" => rand((3..14))
     })
   end
 
   return holidays
+end
+
+def random_cost(hotel)
+  case hotel.stars
+  when 1
+    return rand(40..90)
+  when 2
+    return rand(70..130)
+  when 3
+    return rand(110..180)
+  when 4
+    return rand(160..230)
+  when 5
+    return rand(210..260)
+  end
 end
