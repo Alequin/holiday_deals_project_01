752cf8357c5e01801f9353e033180c037834f077
James Cox <j-cox03@hotmail.com>
2017-06-17 15:00:34 +0100

    Made find by id and find methods in TravelAgent class

--- app/models/travel_agent.rb
+++ app/models/travel_agent.rb
@@ -17,14 +17,25 @@ class TravelAgent < DatabaseAssistant
 
   def TravelAgent.get_all()
     result = DatabaseAssistant.get_all(@@TABLE_NAME)
-    return TravelAgent.map_sql_result(result)
+    return TravelAgent.map_sql_results(result)
   end
 
   def TravelAgent.delete_all()
     DatabaseAssistant.delete_all(@@TABLE_NAME)
   end
 
-  def TravelAgent.map_sql_result(result)
+  def TravelAgent.find_by_id(id)
+    to_find = {"id" => id}
+    result = DatabaseAssistant.find(@@TABLE_NAME, to_find)[0]
+    return TravelAgent.new(result)
+  end
+
+  def TravelAgent.find(query)
+    results = DatabaseAssistant.find(@@TABLE_NAME, query)
+    return TravelAgent.map_sql_results(results)
+  end
+
+  def TravelAgent.map_sql_results(result)
     return result.map(){|agent| TravelAgent.new(agent)}
   end
 
