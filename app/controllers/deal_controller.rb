b4f99e4a8f8f8343a24c76a97ea6df113d4421e9
James Cox <j-cox03@hotmail.com>
2017-06-18 23:38:37 +0100

    Added sorting to some of the option on the view deals page

--- app/controllers/deal_controller.rb
+++ app/controllers/deal_controller.rb
@@ -4,6 +4,8 @@ require("sinatra/contrib/all") if(development?())
 require_relative("../models/deal.rb")
 
 get("/deal") do
-  @deals = Deal.get_all()
+  sort_by = params["sort"]
+  @deals = Deal.get_all(sort_by, "ASC") if(sort_by && sort_by != "nil")
+  @deals = Deal.get_all() if(!@deals)
   erb(:"deal/index")
 end
