6e6e2d1350833d309da557708edcf12d7167723d
James Cox <j-cox03@hotmail.com>
2017-06-18 19:42:35 +0100

    Made basic sorting capabilities on view holidays page (still work to be done)

--- app/controllers/holiday_controller.rb
+++ app/controllers/holiday_controller.rb
@@ -4,6 +4,8 @@ require("sinatra/contrib/all") if(development?())
 require_relative("../models/holiday.rb")
 
 get('/holiday') do
-  @holidays = Holiday.get_all()
+  sort_by = params["sort"]
+  @holidays = Holiday.get_all(sort_by, "ASC") if(sort_by && sort_by != "nil")
+  @holidays = Holiday.get_all() if(!@holidays)
   erb(:"holiday/index")
 end
