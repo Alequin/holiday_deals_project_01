78ee4612cc3322dfc038635266a5c079898bd434
James Cox <j-cox03@hotmail.com>
2017-06-18 23:54:48 +0100

    Replaced linked image with local image due to 403 status from images server. To be done for all travel agent images

--- app/db/seeds/travel_agent_seeds.rb
+++ app/db/seeds/travel_agent_seeds.rb
@@ -19,7 +19,7 @@ def make_travel_agents()
     "reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla " +
     "pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa " +
     "qui officia deserunt mollit anim id est laborum.",
-    "logo_url" => "https://publicdomainvectors.org/photos/logo-33-publicdomainvectors.jpg"
+    "logo_url" => "/images/solace_logo.webp"
   })
 
   agents[1] = TravelAgent.new({
