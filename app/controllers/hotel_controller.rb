770688679c6be393648fd6fee7b791d151cb85b0
James Cox <j-cox03@hotmail.com>
2017-06-18 14:19:51 +0100

    Made basics of sorting in view hotels

--- app/controllers/hotel_controller.rb
+++ app/controllers/hotel_controller.rb
@@ -4,6 +4,7 @@ require("sinatra/contrib/all") if(development?())
 require_relative("../models/hotel.rb")
 
 get('/hotel') do
-  @hotels = Hotel.get_all()
+  @hotels = Hotel.get_all(params["sort"], "ASC") if(params["sort"] && params["sort"] != "nil")
+  @hotels = Hotel.get_all() if(!@hotels)
   erb(:"hotel/index")
 end
