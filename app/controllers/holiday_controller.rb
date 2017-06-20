e090791cfea93abab888d7777f4cbbac324af139
James Cox <j-cox03@hotmail.com>
2017-06-20 17:11:54 +0100

    sorting by hotel name now works on holiday/index.erb

--- app/controllers/holiday_controller.rb
+++ app/controllers/holiday_controller.rb
@@ -7,23 +7,33 @@ require_relative("../models/travel_agent.rb")
 
 get('/holiday') do
   @page_title = "All Holidays"
+  @sort_by_path = "/holiday"
+
+  @holidays = Holiday.get_all()
   sort_by = params["sort"]
-  @holidays = Holiday.get_all(sort_by, "ASC") if(sort_by && sort_by != "nil")
-  @holidays = Holiday.get_all() if(!@holidays)
+  @holidays = Holiday.sort(@holidays, sort_by.to_sym) if(sort_by && sort_by != "no_order")
   erb(:"holiday/index")
 end
 
 get('/travel_agent/:id/holiday') do
   agent = TravelAgent.find_by_id(params["id"])
   @page_title = "Holidays from #{agent.name}"
+  @sort_by_path = "/travel_agent/#{params["id"]}/holiday"
+
   @holidays = Holiday.find({"travel_agent_id" => agent.id})
+  sort_by = params["sort"]
+  @holidays = Holiday.sort(@holidays, sort_by.to_sym) if(sort_by && sort_by != "no_order")
   erb(:"holiday/index")
 end
 
 get('/hotel/:id/holiday') do
   hotel = Hotel.find_by_id(params["id"])
   @page_title = "Holidays at #{hotel.name}"
+  @sort_by_path = "/hotel/#{params["id"]}/holiday"
+
   @holidays = Holiday.find({"hotel_id" => hotel.id})
+  sort_by = params["sort"]
+  @holidays = Holiday.sort(@holidays, sort_by.to_sym) if(sort_by && sort_by != "no_order")
   erb(:"holiday/index")
 end
 
