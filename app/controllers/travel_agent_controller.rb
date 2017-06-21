2be832a076a1d75c15bdf3192f1b0d9b9d3ad6ef
James Cox <j-cox03@hotmail.com>
2017-06-21 10:49:27 +0100

    Removed edit variable from travel_agent route new and edit

--- app/controllers/travel_agent_controller.rb
+++ app/controllers/travel_agent_controller.rb
@@ -11,7 +11,6 @@ get('/travel_agent') do
 end
 
 get('/travel_agent/new') do
-  @edit = false
   erb(:"travel_agent/new")
 end
 
@@ -23,7 +22,6 @@ end
 
 get('/travel_agent/:id/edit') do
   @travel_agent = TravelAgent.find_by_id(params["id"])
-  @edit = true
   erb(:"travel_agent/edit")
 end
 
