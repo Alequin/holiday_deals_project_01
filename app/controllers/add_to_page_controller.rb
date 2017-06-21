fbd3065eea049c0da9c9956b18d9d14b0e6b97bb
James Cox <j-cox03@hotmail.com>
2017-06-21 19:04:27 +0100

    Added labels on routes in add_to_site and deal controllers

--- app/controllers/add_to_page_controller.rb
+++ app/controllers/add_to_page_controller.rb
@@ -1,6 +1,7 @@
 require("sinatra")
 require("sinatra/contrib/all") if(development?())
 
+#INDEX
 get("/add_to_page") do
   erb(:"add_to_page/index")
 end
--- app/controllers/deal_controller.rb
+++ app/controllers/deal_controller.rb
@@ -7,6 +7,7 @@ require_relative("../models/deal.rb")
 require_relative("../models/travel_agent.rb")
 require_relative("../models/hotel.rb")
 
+#INDEX
 get("/deal") do
   @page_title = "All Deals"
   @sort_path = "/deal"
@@ -17,6 +18,7 @@ get("/deal") do
   erb(:"deal/index")
 end
 
+#INDEX WITH SPECIFIC TRAVEL AGENT
 get("/travel_agent/:id/deal") do
   agent = TravelAgent.find_by_id(params["id"])
   @page_title = "Deal with #{agent.name}"
@@ -28,6 +30,7 @@ get("/travel_agent/:id/deal") do
   erb(:"deal/index")
 end
 
+#INDEX WITH SPECIFIC HOTEL
 get("/hotel/:id/deal") do
   hotel = Hotel.find_by_id(params["id"])
   @page_title = "Deal at #{hotel.name}"
@@ -39,24 +42,28 @@ get("/hotel/:id/deal") do
   erb(:"deal/index")
 end
 
+#EDIT
 get("/holiday/:holiday_id/deal/:id/edit") do
   @holiday = Holiday.find_by_id(params["holiday_id"])
   @deal = Deal.find_by_id(params["id"])
   erb(:"deal/edit")
 end
 
+#CREATE
 post("/holidays/:holiday_id/deal") do
   deal = Deal.new(params)
   deal.save()
   redirect to("/holiday/#{params["holiday_id"]}")
 end
 
+#UPDATE
 post("/holiday/:holiday_id/deal/:id") do
   deal = Deal.new(params)
   deal.update()
   redirect to("/holiday/#{deal.holiday_id}")
 end
 
+#DELETE
 post("/deal/:id/delete") do
   deal = Deal.find_by_id(params["id"])
   deal.delete()
